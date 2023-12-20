2d7a9e59f4ac2bfe37d56db9da725295f938a5fa
Gio762002 <guo6437@outlook.com>
2023-12-20 17:51:58 +0100

    verified fcts for reseau

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ print("AS2 id: ",as2.as_id)
 print("AS2 igp: ",as2.igp)
 
 r1 = classr.router("ABR")
+r1.router_id = "1.1.1.1"
 r2 = classr.router("Internal")
+r2.router_id = "2.2.2.2"
 print("r1 type: ",r1.type)
 print("r2 type: ",r2.type)    
 
@@ -26,6 +28,16 @@ print("r1 interfaces: ",r1.interfaces)
 r2eth0 = classr.interface("eth0")
 fctr.init_interface(r2,r2eth0)
 
-#fctr.set_interface(r1,r1eth0,r2,r2eth0)
-#print("r1 all_interfaces: ",r1.all_interfaces)
+fctr.set_interface(r1,r1eth0,r2,r2eth0)
+print("r1 all_interfaces: ",r1.all_interfaces)
 
+fctr.add_router_to_as(r1,as1)
+print("AS1 routers: ",as1.routers)
+
+fctr.delete_link(r1,r2)
+print("r1 all_interfaces: ",r1.all_interfaces)
+print("r1 interfaces: ",r1.interfaces)
+
+
+
+#list_as = [as1,as2] ##need a general list of AS

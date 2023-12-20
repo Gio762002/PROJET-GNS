4244feb9412b027c01f6eae1fbe5d019b13f0c9f
Gio762002 <guo6437@outlook.com>
2023-12-20 15:16:44 +0100

    .

--- class_reseau.py
+++ class_reseau.py
@@ -1,13 +1,13 @@
 class router:
    
-    def __init__(self,type,loopback):
+    def __init__(self,type):
         self.router_id = None
-        self.loopback = loopback
+        self.loopback = None
         self.all_interfaces = {} #interface.name : occupied? (1 or 0)
         #self.routing_table = {} ##maybe not needed
         self.interfaces = {} #interface.name: interface(object)
         self.neighbours = [] #router_id, extrait de self.interface
-        self.type = None # ABR, ASBR, Internal   
+        self.type = type # ABR, ASBR, Internal   
 
 class interface:
     
@@ -20,7 +20,7 @@ class interface:
 
 class anonymous_system:
 
-    def __init__(self, as_id,igp):
+    def __init__(self, as_id, igp):
         self.as_id = as_id
         self.routers = {} # router_id : router(object)
         self.graph = {}

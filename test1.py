21453d2e02ca45471a64ae55863cf3cca0d62587
Gio762002 <guo6437@outlook.com>
2023-12-22 11:49:28 +0100

    验证了直到ipv6地址分配的函数，对于同一as中的路由器的分配无问题，对于连接到其他as的路由器仍会出现重复地址，此外test1中的分配地址代码应转换为函数方便使用

---
+++ test1.py
@@ -0,0 +1,105 @@
+import class_reseau as classr
+import fct_reseau as fctr
+import fct_protocol as fctp
+
+as1 = classr.autonomous_system(1,"OSPF")
+as2 = classr.autonomous_system(2,"RIP")
+as_lst = [as1,as2]
+
+#3 router in AS1
+r1 = classr.router()
+r1.router_id = "1.1.1.1"
+r2 = classr.router("ABR")
+r2.router_id = "2.2.2.2"
+r3 = classr.router("ABR")
+r3.router_id = "3.3.3.3"
+#3 router in AS2
+r4 = classr.router("ABR")
+r4.router_id = "4.4.4.4"
+r5 = classr.router("ABR")
+r5.router_id = "5.5.5.5"
+r6 = classr.router("Internal")
+r6.router_id = "6.6.6.6"
+
+
+#init interfaces
+r1eth0 = classr.interface("eth0")
+r1eth1 = classr.interface("eth1")
+r1eth2 = classr.interface("eth2")
+for interface in [r1eth0,r1eth1,r1eth2]:
+    fctr.init_interface(r1,interface)
+r2eth0 = classr.interface("eth0")
+r2eth1 = classr.interface("eth1")
+r2eth2 = classr.interface("eth2")
+for interface in [r2eth0,r2eth1,r2eth2]:
+    fctr.init_interface(r2,interface)
+r3eth0 = classr.interface("eth0")
+r3eth1 = classr.interface("eth1")
+r3eth2 = classr.interface("eth2")
+for interface in [r3eth0,r3eth1,r3eth2]:
+    fctr.init_interface(r3,interface)
+r4eth0 = classr.interface("eth0")
+r4eth1 = classr.interface("eth1")
+r4eth2 = classr.interface("eth2")
+for interface in [r4eth0,r4eth1,r4eth2]:
+    fctr.init_interface(r4,interface)
+r5eth0 = classr.interface("eth0")
+r5eth1 = classr.interface("eth1")
+r5eth2 = classr.interface("eth2")
+for interface in [r5eth0,r5eth1,r5eth2]:
+    fctr.init_interface(r5,interface)
+r6eth0 = classr.interface("eth0")
+r6eth1 = classr.interface("eth1")
+r6eth2 = classr.interface("eth2")
+for interface in [r6eth0,r6eth1,r6eth2]:
+    fctr.init_interface(r6,interface)
+
+
+for router in [r1,r2,r3]:
+    fctr.add_router_to_as(router,as1)
+for router in [r4,r5,r6]:
+    fctr.add_router_to_as(router,as2)
+
+fctr.as_auto_loopback(as1,"2001:100::")
+fctr.as_auto_loopback(as2,"2001:200::")
+for router in as1.routers.values():
+    fctr.as_loopback_plan(as1)
+for router in as2.routers.values():
+    fctr.as_loopback_plan(as2)
+# print("AS1 loopback plan: ",as2.loopback_plan) checked
+# print(r1.interfaces[r1.loopback_interface].tag)
+
+fctr.local_link(r1,r2)
+# print("r1 all_interfaces: ",r1.all_interfaces)
+# print("r1 interface eth1 connected to: ",r1.interfaces['eth1'].connected_router)
+fctr.local_link(r1,r3)
+fctr.local_link(r2,r4)
+# print("r2 all_interfaces: ",r2.all_interfaces)
+# print("r2 interface eth1 connected to: ",r2.interfaces['eth2'].connected_router)
+fctr.local_link(r3,r5)
+fctr.local_link(r4,r6)
+fctr.local_link(r5,r6)
+
+as1.construct_link_lst()
+as2.construct_link_lst()
+print("AS1 link_lst: ",as1.link_lst)
+# print("AS2 link_lst: ",as2.link_lst)
+
+'''need to convert the code into a function'''
+numero_link = 0
+for ((r1,int1),(r2,int2)) in as1.link_lst: #all input are strings
+    (address1,address2) = fctr.as_auto_addressing(as1,"2001:300::",int1,int2,numero_link)
+    numero_link += 1
+    if r1 in as1.loopback_plan.keys():
+        as1.routers[r1].interfaces[int1].address_ipv6_global = address1
+    elif r1 in as2.loopback_plan.keys():
+        as2.routers[r1].interfaces[int1].address_ipv6_global = address1
+    if r2 in as1.loopback_plan.keys():
+        as1.routers[r2].interfaces[int2].address_ipv6_global = address2
+    elif r2 in as2.loopback_plan.keys():
+        as2.routers[r2].interfaces[int2].address_ipv6_global = address2
+print("r4 interface eth1 address: ",r4.interfaces['eth1'].address_ipv6_global)
+for r in as1.routers.values():
+    for interface in r.interfaces.keys(): #interface as a string
+        print(r.router_id,":",interface,':',r.interfaces[interface].address_ipv6_global)
+    print(" ")
